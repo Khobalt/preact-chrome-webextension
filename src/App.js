1fa9c38dee4685eb6e7cef21e5f5246cf8b8fce1
Lee Dydo <lee@dydo.co>
2021-02-26 11:37:00 -0500

    Slap together a quick bookmark override
    Uses the material-ui preact examples and the dashboard example.

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import Dashboard from './Dashboard.js';
+
+export default function App() {
+  return (
+    // TODO v5: remove once migration to emotion is completed
+    <Dashboard></Dashboard>
+  );
+}
