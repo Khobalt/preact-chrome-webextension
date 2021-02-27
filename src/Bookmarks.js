0c35e69969399ecdec00071df45d460c8dda8113
Lee Dydo <lee@dydo.co>
2021-02-26 19:31:51 -0500

    Get bookmarks onscreen
    The bookmarks are displayed, however unattractively. Long
    urls spill wayyyy to the right. Furthermore, nothing is
    displayed until a refresh by closing the list on the left.

--- src/Bookmarks.js
+++ src/Bookmarks.js
@@ -7,49 +7,39 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Title from './Title';
+import { create } from '@material-ui/core/styles/transitions';
+import { Hidden } from '@material-ui/core';
 
 // Generate Order Data
-function createData(id, date, name, shipTo, paymentMethod, amount) {
-  return { id, date, name, shipTo, paymentMethod, amount };
+function createData(id, date, name, shipTo) {
+  return { id, date, name, shipTo };
 }
 
-const rows = [
-  createData(
-    0,
-    '16 Mar, 2019',
-    'Elvis Presley',
-    'Tupelo, MS',
-    'VISA ⠀•••• 3719',
-    312.44,
-  ),
-  createData(
-    1,
-    '16 Mar, 2019',
-    'Paul McCartney',
-    'London, UK',
-    'VISA ⠀•••• 2574',
-    866.99,
-  ),
-  createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
-  createData(
-    3,
-    '16 Mar, 2019',
-    'Michael Jackson',
-    'Gary, IN',
-    'AMEX ⠀•••• 2000',
-    654.39,
-  ),
-  createData(
-    4,
-    '15 Mar, 2019',
-    'Bruce Springsteen',
-    'Long Branch, NJ',
-    'VISA ⠀•••• 5919',
-    212.79,
-  ),
-];
 
 chrome.bookmarks.getTree(e => console.log(e));
+function getBookmarkArray(id, bookmarkArray) {
+  chrome.bookmarks.getChildren(id, function (children) {
+    children.forEach(function (bookmark) {
+      bookmarkArray.push(bookmark);
+      getBookmarkArray(bookmark.id, bookmarkArray);
+    });
+  });
+  return bookmarkArray;
+}
+
+let bookmarkArray = getBookmarkArray('0', []);
+console.log(bookmarkArray);
+let rows = [];
+bookmarkArray.forEach((item) => {
+  rows.push(createData(
+    item.id,
+    item.title,
+    item.url,
+    item.dateAdded
+  ));
+});
+
+rows = bookmarkArray;
 
 function preventDefault(event) {
   event.preventDefault();
@@ -62,21 +52,17 @@ export default function Bookmarks() {
       <Table size="small">
         <TableHead>
           <TableRow>
-            <TableCell>Date</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Ship To</TableCell>
-            <TableCell>Payment Method</TableCell>
-            <TableCell align="right">Sale Amount</TableCell>
+            <TableCell>Title</TableCell>
+            <TableCell>Url</TableCell>
+            <TableCell>Date Added</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow key={row.id}>
-              <TableCell>{row.date}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.shipTo}</TableCell>
-              <TableCell>{row.paymentMethod}</TableCell>
-              <TableCell align="right">{`$${row.amount}`}</TableCell>
+              <TableCell>{row.title}</TableCell>
+              <TableCell>{row.url}</TableCell>
+              <TableCell>{row.dateAdded}</TableCell>
             </TableRow>
           ))}
         </TableBody>
