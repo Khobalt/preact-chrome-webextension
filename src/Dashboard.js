74ca46827967a8fe93f4a2822425522d2a1ba851
Lee Dydo <lee@dydo.co>
2021-03-01 09:38:21 -0500

    Remove unused function

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -82,8 +82,7 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Dashboard() {
   const classes = useStyles();
-  const [open, setOpen] = React.useState(true);
-
+  
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
